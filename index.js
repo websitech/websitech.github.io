4946371833444cdd67b0c31f8c5cfbfda7539b7d
Virej Dasani <virej@Virejs-MacBook-Air.local>
2021-10-31 13:33:42 +0530

    Styling ++

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-window.onscroll = function () {
-  scrollFunction();
-};
+// For navbar color
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 180 ||
-    document.documentElement.scrollTop > 180
-  ) {
-    document.getElementById("navbar-color").style.background =
-      "rgba(0, 0, 0, 0.89)";
-  } else {
-    document.getElementById("navbar-color").style.background =
-      "rgba(0, 0, 0, 0.31)";
-  }
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 180 ||
+//     document.documentElement.scrollTop > 180
+//   ) {
+//     document.getElementById("navbar-color").style.background =
+//       "rgba(0, 0, 0, 0.89)";
+//   } else {
+//     document.getElementById("navbar-color").style.background =
+//       "rgba(0, 0, 0, 0.31)";
+//   }
+// }
+
+if (window.innerWidth < 500) {
+  console.log("less");
+  document.getElementById("illustration").style.display = "none";
+} else {
+  document.getElementById("illustration").style.display = "block";
 }
